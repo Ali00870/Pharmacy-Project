3c6cdaa6d83a27a1fdca052f506114ba59724d6f
MohamedGadwal <s-mohamed.gadwal@zewailcity.edu.eg>
2024-12-20 02:15:41 +0200

    Linkind el BD trial1

---
+++ Models/PharmacyDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Pharmacy_back.Models
+{
+    public class PharmacyDB
+    {
+        private string ConnectionString = "Data Source =DESKTOP-O1HOQTT\\SQLEXPRESS01 ; Initial Catlog= sydality ; Integrated Security = True ; Trust Server Certificate = True  ";
+        public SqlConnection con { get; set; }
+        public PharmacyDB()
+        {
+            con = new SqlConnection(ConnectionString);
+        }
+    }
+}
+ 
